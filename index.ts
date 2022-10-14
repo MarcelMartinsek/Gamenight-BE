7e48e760ca18d0b0349ac03b1d7fe4f9fbffad5d
Marcel Martinšek <mm0445@student.uni-lj.si>
2022-10-14 23:59:18 +0200

    .env variables accessible

--- index.ts
+++ index.ts
@@ -1,38 +1,41 @@
 import fastify, { FastifyInstance } from "fastify";
 
-const app: FastifyInstance = fastify( {logger: true} );
+const app: FastifyInstance = fastify({ logger: true });
 
-interface IQueryInterface{
-    username: string;
-    password: string;
-};
+interface IQueryInterface {
+  username: string;
+  password: string;
+}
 
-interface IHeaders{
-    'x-access-token': string;
-};
+interface IHeaders {
+  "x-access-token": string;
+}
 
-interface IReply{
-    code: number;
-    message: string;
-    body: any;
-};
+interface IReply {
+  code: number;
+  message: string;
+  body: any;
+}
 
-app.get<{Querystring: IQueryInterface, Headers: IHeaders, Reply: IReply}>('/', (request, reply) => {
-    const {username, password} = request.query;
+app.get<{ Querystring: IQueryInterface; Headers: IHeaders; Reply: IReply }>(
+  "/",
+  (request, reply) => {
+    const { username, password } = request.query;
     reply.send({
-        code: 200,
-        message: 'success',
-        body: {
-            username,
-            password
-        }
+      code: 200,
+      message: "success",
+      body: {
+        username,
+        password,
+      },
     });
-});
+  }
+);
 
-app.listen({ port: 3000, host: '127.0.0.1' }, (err, address) => {
-    if(err){
-        app.log.error(err);
-        process.exit(1);
-    }
-    // app.log.info(`server listening on ${address}`);
+app.listen({ port: 3000, host: "127.0.0.1" }, (err, address) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+  // app.log.info(`server listening on ${address}`);
 });
